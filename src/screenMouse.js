7becc323dab984814f63fdc6f9ebfcc8ccbc9250
Sander Moolin <sandermoolin@gmail.com>
2019-02-13 15:00:17 -0500

    added screenMouse mixin

---
+++ src/screenMouse.js
@@ -0,0 +1,44 @@
+import { schedule, listen, everyFrame, transform } from 'popmotion'
+const { interpolate } = transform
+
+export default (opts = {}) => {
+    // build out default opts
+    opts = {
+        bottomLeft: { x: 0, y: 0 },
+        topRight: { x: 1, y: 1 },
+        ...opts
+    }
+
+    return {
+        data() {
+            return {
+                screenMouseScheduler: null,
+                screenMouse: { x: 0, y: 0 }
+            }
+        },
+        mounted() {
+            this.screenMouseScheduler = schedule(
+                everyFrame(),
+                listen(window, 'mousemove')
+            )
+                .pipe(e => {
+                    return { x: e.clientX, y: e.clientY }
+                })
+                .start(({ x, y }) => {
+                    this.screenMouse.x = interpolate(
+                        [0, window.innerWidth],
+                        [opts.bottomLeft.x, opts.topRight.x]
+                    )(x)
+                    this.screenMouse.y = interpolate(
+                        [window.innerHeight, 0],
+                        [opts.bottomLeft.y, opts.topRight.y]
+                    )(y)
+                })
+        },
+        beforeDestroy() {
+            if (this.screenMouseScheduler) {
+                this.screenMouseScheduler.stop()
+            }
+        }
+    }
+}
